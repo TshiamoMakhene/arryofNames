0b93121e9068bbb8c8df16efbe60f5657c1ba067
TJ Makhene <tshiamojmakhene@gmail.com>
2024-08-01 09:02:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace arryofNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userName = new string[5];
+            
+            for (int i =0; i < userName.Length; i++)
+            {
+                Console.WriteLine($"Please enter five names {i+1} :");
+               userName[i] = Console.ReadLine();
+                
+            }
+            Console.WriteLine( "The names you have entered are :");
+            foreach (string user in userName)
+            {
+                Console.WriteLine( user);
+            }
+        }
+    }
+}
